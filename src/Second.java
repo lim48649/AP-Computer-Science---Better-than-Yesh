d7a07b9070bf3ccbaaf916a729f72f630f3005ed
Yesh Chandiramani <yesh0907@hotmail.com>
2016-08-11 19:57:51 +0800

    Did program first and second

---
+++ src/Second.java
@@ -0,0 +1,11 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+		int x = 5;
+		System.out.println("The value of x is " + x);
+		x = x + 10;
+		System.out.println("The updated value of x is " + x);
+	}
+
+}
